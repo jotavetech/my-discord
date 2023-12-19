57d837eceb462dc1bfc27a03241ffacf6403e112
jotavetech <ojoaogugel@gmail.com>
2023-12-19 11:02:05 -0300

    authentication with clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div className="flex flex-col">This is a protecteed route.</div>;
+  return (
+    <div className="flex flex-col">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
