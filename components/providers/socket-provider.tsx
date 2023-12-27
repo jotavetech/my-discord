7352db2debcdf064ede879cc183d18547846eae1
jotavetech <ojoaogugel@gmail.com>
2023-12-27 10:42:44 -0300

    socket io setup

---
+++ components/providers/socket-provider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { io as ClientIO } from "socket.io-client";
+
+type SocketContextType = {
+  socket: any | null;
+  connected: boolean;
+};
+
+const SocketContext = createContext<SocketContextType>({
+  socket: null,
+  connected: false,
+});
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [socket, setSocket] = useState(null);
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    const socketInstance = new (ClientIO as any)(
+      process.env.NEXT_PUBLIC_SITE_URL!,
+      {
+        path: "/api/socket/io",
+        addTrailingSlash: false,
+      }
+    );
+
+    socketInstance.on("connect", () => setConnected(true));
+    socketInstance.on("disconnect", () => setConnected(false));
+
+    setSocket(socketInstance);
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, connected }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
