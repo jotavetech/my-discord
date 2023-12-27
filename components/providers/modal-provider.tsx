41cfe6833002f2d1f8a875f0f7dcd58e65f1bb9e
jotavetech <ojoaogugel@gmail.com>
2023-12-26 22:09:19 -0300

    edit & delete channels

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,7 +8,9 @@ import InviteModal from "@/components/modals/invite-modal";
 import MembersModal from "@/components/modals/members-modal";
 import CreateChannelModal from "@/components/modals/create-channel-modal";
 import LeaveModal from "@/components/modals/leave-modal";
-import DeleteModal from "../modals/delete-modal";
+import DeleteModal from "@/components/modals/delete-modal";
+import DeleteChannelModal from "@/components/modals/delete-channel-modal";
+import EditChannelModal from "../modals/edit-channel-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -28,6 +30,8 @@ const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveModal />
       <DeleteModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -8,6 +8,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 
 import { useParams, useRouter } from "next/navigation";
 import ActionTooltip from "../action-tooltip";
+import useModal from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -22,6 +23,8 @@ const iconMap = {
 };
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
+  const { onOpen } = useModal();
+
   const params = useParams();
   const router = useRouter();
 
@@ -48,10 +51,16 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
       {channel.name !== "general" && role !== MemberRole.GUEST && (
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
-            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Edit
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+              onClick={() => onOpen("editChannel", { channel, server })}
+            />
           </ActionTooltip>
           <ActionTooltip label="Delete">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Trash
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+              onClick={() => onOpen("deleteChannel", { channel, server })}
+            />
           </ActionTooltip>
         </div>
       )}
